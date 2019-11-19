85bdefe23a4871eb07cb60f6b8676af0907294d5
Patrick Gustafson <patrickgus@charter.net>
2019-11-19 07:56:24 -0800

    configure left and right splits

--- src/composition/Split.js
+++ src/composition/Split.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Split.css';
 
-function Split() {
+function Split(props) {
+  const combinedClassName = `split ${props.className}`;
+  const newStyles = { flex: props.flexBasis };
   return (
-    <div className="split">
-      This is the content of the split component. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+    <div 
+      className={combinedClassName}
+      style={newStyles}  
+    >
+      {props.children}
     </div>
   );
 }
