25da28aa847736f409f9a74063fb20b3afb2d578
Patrick Gustafson <patrickgus@charter.net>
2019-11-19 08:13:02 -0800

    add 2 Tooltip variables

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import Split from './composition/Split';
 import './App.css';
 import Tooltip from './composition/Tooltip';
 
+// make 2 tooltips here and another inside the App directly
+const firstTooltip = (
+  <Tooltip message='tooltip message'>
+   ipsum
+  </Tooltip>
+);
+const secondTooltip = (
+  <Tooltip message='another tooltip message'>
+    officiis
+  </Tooltip>
+);
+
 function App() {
   return (
     <main className='App'>
