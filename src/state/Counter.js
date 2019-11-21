efebf5c85493b41953677e64409217493515c529
Patrick Gustafson <patrickgus@charter.net>
2019-11-21 09:31:17 -0800

    add console logs

--- src/state/Counter.js
+++ src/state/Counter.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 class Counter extends React.Component {
   constructor(props) {
+    console.log('props in constructor', props);
     super(props);
-    this.state = {
-      count: 0
-    };
+    this.state = { count: 0 }
   }
   handleButtonClick = () => {
+    console.log('props in handleButtonClick', this.props);
+    console.log('state in handleButtonClick', this.state);
     const newCount = this.state.count + 1;
     this.setState({
       count: newCount
