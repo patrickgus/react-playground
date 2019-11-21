5d20fcafdac53a361f52f141cf03435fbe38a36b
Patrick Gustafson <patrickgus@charter.net>
2019-11-21 08:57:37 -0800

    change to only include Counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Messages from './Messages';
-import TheDate from './state/TheDate';
+import Counter from './state/Counter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TheDate />
-        <h1>YOUR APPLICATION NAME!</h1>
-        <Messages name="Messages" unread={0} />
-        <Messages name="Notifications" unread={10} />
+        <Counter />
       </div>
     );
   }
