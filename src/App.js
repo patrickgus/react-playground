42a1ca467d9f57af99b7ffb3f04171e59dff234e
Patrick Gustafson <patrickgus@charter.net>
2019-11-21 10:05:43 -0800

    add HelloWorld component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import Counter from './state/Counter';
+// import Counter from './state/Counter';
+import HelloWorld from './state-drills/HelloWorld';
 
 class App extends Component {
-  static defaultProps = {
-    step: 1
-  }
+  // static defaultProps = {
+  //   step: 1
+  // }
   render() {
     return (
       <div className="App">
-        <Counter count={123} step/>
+        {/* <Counter count={123} step/> */}
+        <HelloWorld />
       </div>
     );
   }
