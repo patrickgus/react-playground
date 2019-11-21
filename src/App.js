80b3e4e54f50637caeb527d84c2a7e49590cc0eb
Patrick Gustafson <patrickgus@charter.net>
2019-11-21 11:17:20 -0800

    add RouletteGun component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 // import Counter from './state/Counter';
 // import HelloWorld from './state-drills/HelloWorld';
-import Bomb from './state-drills/Bomb';
+// import Bomb from './state-drills/Bomb';
+import RouletteGun from './state-drills/RouletteGun';
 
 class App extends Component {
   // static defaultProps = {
@@ -13,7 +14,8 @@ class App extends Component {
       <div className="App">
         {/* <Counter count={123} step/> */}
         {/* <HelloWorld /> */}
-        <Bomb />
+        {/* <Bomb /> */}
+        <RouletteGun />
       </div>
     );
   }
---
+++ src/state-drills/RouletteGun.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+export default class RouletteGun extends React.Component {
+  static defaultProps = {
+    bulletInChamber: 8
+  }
+
+  state = {
+    chamber: null,
+    spinningTheChamber: false
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
+
+  handleButtonClick = () => {
+    this.setState({
+      spinningTheChamber: true
+    });
+    this.timeout = setTimeout(() => {
+      const newChamber = Math.ceil(Math.random() * 8);
+      this.setState({
+        chamber: newChamber,
+        spinningTheChamber: false
+      });
+    }, 2000);
+  }
+
+  renderDisplay() {
+    const { chamber, spinningTheChamber } = this.state;
+    const { bulletInChamber } = this.props;
+
+    if(spinningTheChamber) {
+      return 'spinning the chamber and pulling the trigger! ...';
+    } else if(chamber === bulletInChamber) {
+      return 'BANG!!!!';
+    } else {
+      return `you're safe!`;
+    }
+  }
+  
+  render() {
+    return (
+      <div className="Gun">
+        <p>{this.renderDisplay()}</p>
+        <button onClick={this.handleButtonClick}>
+          Pull the trigger!
+        </button>
+      </div>
+    );
+  }
+}
