d1edfe258482fe5deb26f0fa7e3ce9c05399afd1
Patrick Gustafson <patrickgus@charter.net>
2019-11-21 13:59:54 -0800

    initiate Counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-// import Counter from './state/Counter';
+import Counter from './state/Counter';
 // import HelloWorld from './state-drills/HelloWorld';
 // import Bomb from './state-drills/Bomb';
 // import RouletteGun from './state-drills/RouletteGun';
-import Tabs from './state/Tabs';
+// import Tabs from './state/Tabs';
 
 const tabsProp = [
   { name: 'First tab',
@@ -19,11 +19,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Counter count={123}/> */}
+        <Counter count={123}/>
         {/* <HelloWorld /> */}
         {/* <Bomb /> */}
         {/* <RouletteGun /> */}
-        <Tabs tabs={tabsProp} />
+        {/* <Tabs tabs={tabsProp} /> */}
       </div>
     );
   }
