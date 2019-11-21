2708fc1e65b6ac65e71602c573e37292019c55d2
Patrick Gustafson <patrickgus@charter.net>
2019-11-21 07:58:28 -0800

    add TheDate component as state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Messages from './Messages';
+import TheDate from './state/TheDate';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <TheDate />
         <h1>YOUR APPLICATION NAME!</h1>
         <Messages name="Messages" unread={0} />
         <Messages name="Notifications" unread={10} />
