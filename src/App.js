adfdcbb6dd3bcdfe3fe8cd12ba91c0a2a3bb41d3
Patrick Gustafson <patrickgus@charter.net>
2019-12-04 09:18:15 -0800

    implement demonym app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React, { Component } from 'react';
-import './App.css';
-import Counter from './state/Counter';
+import React, { Component } from "react";
+import "./App.css";
+// import Counter from './state/Counter';
 // import HelloWorld from './state-drills/HelloWorld';
 // import Bomb from './state-drills/Bomb';
 // import RouletteGun from './state-drills/RouletteGun';
 // import Tabs from './state/Tabs';
+import DemonymApp from "./demonymapp/demonymApp";
 
-const tabsProp = [
-  { name: 'First tab',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
-  { name: 'Second tab',
-    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
-  { name: 'Third tab',
-    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' }
-];
+// const tabsProp = [
+//   { name: 'First tab',
+//     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+//   { name: 'Second tab',
+//     content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+//   { name: 'Third tab',
+//     content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' }
+// ];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter count={123}/>
+        {/* <Counter count={123}/> */}
         {/* <HelloWorld /> */}
         {/* <Bomb /> */}
         {/* <RouletteGun /> */}
         {/* <Tabs tabs={tabsProp} /> */}
+        <DemonymApp />
       </div>
     );
   }
---
+++ src/demonymapp/demonymApp.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import "./demonymApp.css";
+
+import Demonym from "./demonym";
+import CountrySelector from "./countrySelector";
+
+class DemonymApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countries: [],
+      selected: null
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://country.register.gov.uk/recordsWRONG.json?page-size=5000")
+      .then(response => {
+        console.log("About to check for errors");
+        if (!response.ok) {
+          console.log("An error did occur, let's throw an error.");
+          throw new Error("Something went wrong");
+        }
+        return response;
+      })
+      .then(response => response.json())
+      .then(data => {
+        const countries = Object.keys(data).map(key => data[key].item[0]);
+        this.setState({
+          countries
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+  }
+
+  setSelected(selected) {
+    this.setState({
+      selected
+    });
+  }
+
+  render() {
+    const demon = this.state.selected ? (
+      <Demonym
+        name={this.state.selected["citizen-names"]}
+        country={this.state.selected.name}
+      />
+    ) : (
+      <div className="demonym_app__placeholder">Select a country above</div>
+    );
+
+    const error = this.state.error ? (
+      <div className="demonym_app__error">{this.state.error}</div>
+    ) : (
+      ""
+    );
+
+    return (
+      <div className="demonym_app">
+        {error}
+        <CountrySelector
+          countries={this.state.countries}
+          changeHandler={selected => this.setSelected(selected)}
+        />
+        {demon}
+      </div>
+    );
+  }
+}
+
+export default DemonymApp;
