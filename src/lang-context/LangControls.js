07af51c86e51c0ffaf7c9bc5e51b96db0bd20bea
Patrick Gustafson <patrickgus@charter.net>
2019-12-07 14:36:40 -0800

    add disable functionality

--- src/lang-context/LangControls.js
+++ src/lang-context/LangControls.js
@@ -1,26 +1,33 @@
 import React from "react";
+import LanguageContext from "./LanguageContext";
 
 export default function LangControls(props) {
   return (
-    <>
-      <button>
-        British{" "}
-        <span role="img" aria-label="en-GB">
-          🇬🇧
-        </span>
-      </button>{" "}
-      <button>
-        American{" "}
-        <span role="img" aria-label="en-US">
-          🇺🇸
-        </span>
-      </button>{" "}
-      <button>
-        Korean{" "}
-        <span role="img" aria-label="ko">
-          🇰🇷
-        </span>
-      </button>
-    </>
+    <LanguageContext.Consumer>
+      {value => {
+        return (
+          <>
+            <button disabled={value.lang === "en-GB"}>
+              British{" "}
+              <span role="img" aria-label="en-GB">
+                🇬🇧
+              </span>
+            </button>{" "}
+            <button disabled={value.lang === "en-US"}>
+              American{" "}
+              <span role="img" aria-label="en-US">
+                🇺🇸
+              </span>
+            </button>{" "}
+            <button disabled={value.lang === "ko"}>
+              Korean{" "}
+              <span role="img" aria-label="ko">
+                🇰🇷
+              </span>
+            </button>
+          </>
+        );
+      }}
+    </LanguageContext.Consumer>
   );
 }
