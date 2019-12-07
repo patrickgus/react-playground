2955c23e21c0f33ee11040dcf3f0dc81b5f1d1db
Patrick Gustafson <patrickgus@charter.net>
2019-12-07 13:46:38 -0800

    import AppLang

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import App from './paltrows-power-toes/App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import AppLang from "./lang-context/AppLang";
+import "./index.css";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<AppLang />, document.getElementById("root"));
