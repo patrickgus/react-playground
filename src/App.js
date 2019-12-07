a19cac23b35d2de183d2ec173dfc601da9f6ea0a
Patrick Gustafson <patrickgus@charter.net>
2019-12-07 09:44:18 -0800

    empty App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-import DemonymApp from "./demonymapp/demonymApp";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <DemonymApp />
       </div>
     );
   }
