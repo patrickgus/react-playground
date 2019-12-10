33060bfd415e7331f3200eb74c6da33215f9222e
Patrick Gustafson <patrickgus@charter.net>
2019-12-10 14:10:06 -0800

    import RegistrationForm component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AppLang from "./lang-context/AppLang";
+import RegistrationForm from "./registrationForm/registrationForm";
 import "./index.css";
 
-ReactDOM.render(<AppLang />, document.getElementById("root"));
+ReactDOM.render(<RegistrationForm  />, document.getElementById("root"));
